00363818116d169fd0040a7e11984d3145387b20
Ben <Ben@10.1.10.213>
2023-05-15 13:40:39 -0400

    Comment is removed, adding new method

--- GitHubProject/src/test/java/com/practic/git/HelloWorld.java
+++ GitHubProject/src/test/java/com/practic/git/HelloWorld.java
@@ -2,8 +2,11 @@ package com.practic.git;
 
 public class HelloWorld {
 	public static void main(String[] args) {
-		//printing hello word string in the console
+		
 		System.out.println("hello word");
+		
+	}
+	public void emptyMethod() {
 		
 	}
 
