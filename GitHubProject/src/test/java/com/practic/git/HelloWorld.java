7d03880e4c47cf00a7d90d288290719a88e00cd3
Ben <Ben@10.1.10.213>
2023-05-15 15:23:29 -0400

    Updated empty method to addtion method

--- GitHubProject/src/test/java/com/practic/git/HelloWorld.java
+++ GitHubProject/src/test/java/com/practic/git/HelloWorld.java
@@ -4,9 +4,13 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		
 		System.out.println("hello word");
-		
+		HelloWorld obj = new HelloWorld();
+		obj.addition(3, 4);
 	}
-	public void emptyMethod() {
+	public void addition(int x, int y) {
+		int result = x+y;
+		System.out.println("num1= "+x +"num2="+y);
+		System.out.println("x+y="+result);
 		
 	}
 
