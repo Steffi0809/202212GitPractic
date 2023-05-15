03861e02ef2b239973f98bef7e13a3b6b24f8f6a
Ben <Ben@10.1.10.213>
2023-05-15 13:36:08 -0400

    Adding printing on console

--- GitHubProject/src/test/java/com/practic/git/HelloWorld.java
+++ GitHubProject/src/test/java/com/practic/git/HelloWorld.java
@@ -1,5 +1,10 @@
 package com.practic.git;
 
 public class HelloWorld {
+	public static void main(String[] args) {
+		//printing hello word string in the console
+		System.out.println("hello word");
+		
+	}
 
 }
